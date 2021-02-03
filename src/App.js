65c92c05fd3db39d1c5b7691fbb23a9513c8b226
Nicholas Renotte <nicholas.renotte@gmail.com>
2021-02-03 20:16:29 +1100

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+// https://github.com/tensorflow/tfjs-models/tree/master/speech-commands
 
-function App() {
+// 0. Import depdendencies
+import * as tf from "@tensorflow/tfjs"
+import * as speech from "@tensorflow-models/speech-commands"
+
+
+const App = () => {
+// 1. Create model and action states
+const [model, setModel] = useState(null)
+const [action, setAction] = useState(null)
+const [labels, setLabels] = useState(null) 
+
+// 2. Create Recognizer
+const loadModel = async () =>{
+  const recognizer = await speech.create("BROWSER_FFT")
+  console.log('Model Loaded')
+  await recognizer.ensureModelLoaded();
+  console.log(recognizer.wordLabels())
+  setModel(recognizer)
+  setLabels(recognizer.wordLabels())
+}
+
+useEffect(()=>{loadModel()}, []); 
+
+// 
+function argMax(arr){
+  return arr.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+}
+
+// 3. Listen for Actions
+const recognizeCommands = async () =>{
+  console.log('Listening for commands')
+  model.listen(result=>{
+    // console.log(labels[argMax(Object.values(result.scores))])
+    console.log(result.spectrogram)
+    setAction(labels[argMax(Object.values(result.scores))])
+  }, {includeSpectrogram:true, probabilityThreshold:0.9})
+  setTimeout(()=>model.stopListening(), 10e3)
+}
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +49,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button onClick={recognizeCommands}>Command</button>
+          {action ? <div>{action}</div>:<div>No Action Detected</div> }
       </header>
     </div>
   );
